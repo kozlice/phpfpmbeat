e2ab6d0285c3828970607726d595a3a8d24be78a
Valentin Nazarov <rusarmy@gmail.com>
2015-11-29 00:15:23 +0300

    Remove redundant comments

--- publisher/publisher.go
+++ publisher/publisher.go
@@ -7,17 +7,14 @@ import (
 	"github.com/elastic/libbeat/publisher"
 )
 
-// StubPublisher is a Publisher that publishes Nginx Stub status.
 type PhpfpmPublisher struct {
 	client publisher.Client
 }
 
-// NewPhpfpmPublisher constructs a new PhpfpmPublisher.
 func New(c publisher.Client) *PhpfpmPublisher {
 	return &PhpfpmPublisher{client: c}
 }
 
-// Publish Phpfpm status.
 func (pfb *PhpfpmPublisher) Publish(s map[string]interface{}) {
 	pfb.client.PublishEvent(common.MapStr{
 		"@timestamp": common.Time(time.Now()),
